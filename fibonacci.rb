aaf828601b9c9444ac83b19baa92e31c157cd65e
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-08-26 19:05:34 -0700

    Add fibonacci solutions

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,3 +1,19 @@
 # frozen_string_literal: true
 
-def fibonacci_iteration(n); end
+def fibs(num)
+  res = []
+  (num + 1).times do |i|
+    res << i if i.between?(0, 1)
+    res << (res[i - 1] + res[i - 2]) if i > 1
+  end
+  res
+end
+
+def fibs_rec(num, res = [], counter = 0)
+  res << counter if counter.between?(0, 1)
+  res << res[counter - 2] + res[counter - 1] if counter > 1
+  return res if num.eql?(counter)
+
+  fibs_rec(num, res, counter + 1)
+  res
+end
